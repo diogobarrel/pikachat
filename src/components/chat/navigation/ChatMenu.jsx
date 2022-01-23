1e5a3ade8bf37f9a99b20b51a30bd282a9e7e6b1
diogobarrel <santos.barrel@gmail.com>
2022-01-23 20:20:40 -0300

    chore: bind menu itens to store

--- src/components/chat/navigation/ChatMenu.jsx
+++ src/components/chat/navigation/ChatMenu.jsx
@@ -1,84 +1,39 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import ChatMenuItem from './ChatMenuItem'
 import './ChatMenu.scss'
 import { TextField } from '@mui/material'
+import { useSelector } from 'react-redux'
 
-export default class ChatMenu extends Component {
-    constructor(props) {
-        super(props)
+export default function ChatMenu() {
+  const [filter, setFilter] = useState('')
 
-        this.state = {
-            chats: [
-                {
-                    id: 1,
-                    url: '',
-                    text: 'Olá! Bem vindo ao pikachat!',
-                    name: 'Joao',
-                },
-                {
-                    id: 2,
-                    url: '',
-                    text: 'Olá! Bem vindo ao pikachat!',
-                    name: 'Mario',
-                },
-                {
-                    id: 3,
-                    url: '',
-                    text: 'Olá! Bem vindo ao pikachat!',
-                    name: 'Clara',
-                },
-                {
-                    id: 4,
-                    url: '',
-                    text: 'Olá! Bem vindo ao pikachat!',
-                    name: 'Afonso',
-                },
-                {
-                    id: 5,
-                    url: '',
-                    text: 'Olá! Bem vindo ao pikachat!',
-                    name: 'Rafa',
-                },
-            ],
-            activeChatt: {
-                id: '',
-            },
-            chatFilter: '',
-        }
-    }
+  const handleChatFilterInput = (event) => {
+    setFilter(event.target.value)
+  }
 
-    handleChatFilterInput = (event) => {
-        this.setState({ chatFilter: event.target.value })
-    }
+  const chats = useSelector((state) => state.chatStore.chats)
 
-    render() {
-        return (
-            <div className="chat-menu">
-                <div className="chat-menu__header">
-                    {' '}
-                    <h3>Conversas</h3>{' '}
-                </div>
-                <div className="chat-menu-list">
-                    <div className="chat-menu-list__filter">
-                        <TextField
-                            id="outlined-name"
-                            label="Name"
-                            value={this.state.chatFilter}
-                            onChange={this.handleChatFilterInput}
-                        >
-                            {}
-                        </TextField>
-                    </div>
-                    <div className="chat-menu-list__items">
-                        {this.state.chats.map((chat) => (
-                            <ChatMenuItem
-                                key={chat.id}
-                                chat={chat}
-                            ></ChatMenuItem>
-                        ))}
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  return (
+    <div className="chat-menu">
+      <div className="chat-menu__header">
+        <h3>Conversas</h3>
+      </div>
+      <div className="chat-menu-list">
+        <div className="chat-menu-list__filter">
+          <TextField
+            id="outlined-name"
+            label="Name"
+            value={filter}
+            onChange={handleChatFilterInput}
+          ></TextField>
+        </div>
+        <div className="chat-menu-list__items">
+          {chats &&
+            chats.map((chat) => (
+              <ChatMenuItem key={chat.id} chat={chat}></ChatMenuItem>
+            ))}
+        </div>
+      </div>
+    </div>
+  )
 }
