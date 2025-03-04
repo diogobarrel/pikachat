2e0e984f41bc08f98c53ad876824f251f69c8154
diogobarrel <santos.barrel@gmail.com>
2025-03-04 13:48:17 -0300

    chore: setup webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,16 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
 module.exports = {
-  // other configurations...
+  entry: './src/index.js', // Adjust the entry point as needed
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  devtool: 'source-map',
   module: {
     rules: [
       // other rules...
@@ -11,6 +22,66 @@ module.exports = {
           /node_modules[\\/]htmlparser2[\\/]node_modules[\\/]entities[\\/]lib[\\/]generated[\\/]encode-html\.js/
         ],
       },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader', // Injects styles into DOM
+          'css-loader',   // Translates CSS into CommonJS
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader', // Injects styles into DOM
+          'css-loader',   // Translates CSS into CommonJS
+          'sass-loader',  // Compiles SASS to CSS
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.svg$/,
+        use: {
+          loader: 'svg-url-loader',
+          options: {
+            iesafe: true,
+          },
+        },
+      },
     ],
   },
-}
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // Path to your HTML template
+    }),
+    new Dotenv(), // Add Dotenv plugin
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
+    historyApiFallback: true,
+  },
+};
